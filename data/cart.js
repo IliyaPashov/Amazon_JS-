23660565631d656e25dc320a62dd3564a0c39195
Iliya <iliq757@gmail.com>
2025-10-06 19:44:36 +0300

    move add to cart to cart

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach.Each((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+
+    }); 
+
+    if (matchingItem) { 
+        matchingItem.quantity += 1;
+    
+    } else { 
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+}
