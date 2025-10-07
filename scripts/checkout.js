6217bb1a51da983a5a58b45e9a3c14f253317ee7
Iliya <iliq757@gmail.com>
2025-10-07 22:59:01 +0300

    save cart to local storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
 
    
      cartSumaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
          <div class="cart-item-container
       js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
@@ -109,5 +110,9 @@ document.querySelectorAll('.js-cart-items')
      removeFromCart(productId);
       console.log(productId);
       
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
 });
