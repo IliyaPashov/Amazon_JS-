b33ea68ed3b247c0f933719cdda7db19073af2e3
Iliya <iliq757@gmail.com>
2025-10-09 01:28:05 +0300

    save data adn generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+const deliveryOption = [{
+    id: '1',
+    deliryDays: 7,
+    priceCents: 0
+}, {
+    id: '2',
+    deliryDays: 3,
+    priceCents: 499
+}, {
+    id: '3',
+    deliryDays: 1,
+    priceCents: 999
+}];
