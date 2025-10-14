d34719c609714b0a8e3c848ee28633ce676f8244
Iliya <iliq757@gmail.com>
2025-10-14 20:49:41 +0300

    test framework jasmine

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,6 +1,6 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
-console.log ('test suite:format ');
+console.log ('test suite:formatCurrency');
 console.log('converting cents to dollars');
 if (formatCurrency(2095) === '20.95') {
     console.log('passed');
