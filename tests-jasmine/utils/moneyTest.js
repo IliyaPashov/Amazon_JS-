9b63250f9f4dab5aa3853050eb4cc4a7e2c31c2a
Iliya <iliq757@gmail.com>
2025-10-14 23:38:45 +0300

    test cart Qty

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('test suite: Format curency', () => {
     it('converting cents to dollars', () => {
